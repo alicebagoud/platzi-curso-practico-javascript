850c2be2541e224e3ac2a14ca4224f9d55805957
alicebagoud <alicebagoud@hotmail.com>
2022-05-24 22:09:06 -0500

    main

--- descuentos.js
+++ descuentos.js
@@ -15,13 +15,70 @@ function onClickButtonPriceDiscount() {
   const inputDiscount = document.getElementById("InputDiscount");
   const discountValue = inputDiscount.value;
 
-  const precioConDescuento = calcularPrecioConDescuento(
-    priceValue,
-    discountValue
-  );
+  const inputCoupon = document.getElementById("InputCoupon");
+  const couponValue = inputCoupon.value;
 
-  const resultP = document.getElementById("ResultP");
-  resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+  /*   const coupons = ["El mejor descuento", "Un buen descuento", "Ça va"];
+   */
+  let descuento;
+
+  // SOLUCION 1
+
+  /*   switch (couponValue) {
+    case coupons[0]: // "El mejor descuento"
+      descuento = 70;
+      break;
+    case coupons[1]: // "Un buen descuento"
+      descuento = 50;
+      break;
+    case coupons[2]: // "Ça va";
+      descuento = 25;
+      break;
+  } */
+
+  // SOLUCION 2
+
+  /*   if (!coupons.includes(couponValue)) {
+    alert("El cupón " + couponValue + "no es válido");
+  } else if (couponValue === "El mejor descuento") {
+    descuento = 70;
+  } else if (couponValue === "Un buen descuento") {
+    descuento = 50;
+  } else if (couponValue === "Ça va") {
+    descuento = 25;
+  } */
+
+  const coupons = [
+    {
+      name: "El mejor descuento",
+      descuento: 70,
+    },
+    {
+      name: "Un buen descuento",
+      descuento: 50,
+    },
+    {
+      name: "Ça va",
+      descuento: 25,
+    },
+  ];
+
+  const isCouponValueValid = function (coupon) {
+    return coupon.name === couponValue;
+  };
+  const userCoupon = coupons.find(isCouponValueValid);
+
+  if (!userCoupon) {
+    alert("El coupon " + couponValue + " no es válido");
+  } else {
+    const descuento = userCoupon.descuento;
+    const precioConDescuento = calcularPrecioConDescuento(
+      priceValue,
+      descuento
+    );
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento es de: $" + precioConDescuento;
+  }
 }
 
 /* console.log({
